fe31df13844253933835225dc8f722723974ec76
SolKuczala <sol.kuczala@gmail.com>
2019-10-29 16:01:38 +0100

    random_character.. prints a series of characters

--- parser.py
+++ parser.py
@@ -1,7 +1,11 @@
 
+from random import choice
+from string import ascii_letters
+
 def random_characters_returner(number):
     "Returns a string of character with lenght in number form provided."
-    pass
+    for number in range(number):
+        print(choice(ascii_letters))
 
 def string_enconder_64(string):
     "Returns a string encoded in base64."
@@ -9,3 +13,5 @@ def string_enconder_64(string):
 def decoder_64(encoded_string):
     "Returns an encoded base64 string decoded."
     pass
+
+random_characters_returner(5)
