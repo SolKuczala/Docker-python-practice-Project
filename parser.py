32e01cf769f96db842074df2ace416d6fe0e84a2
SolKuczala <sol.kuczala@gmail.com>
2019-10-29 13:32:30 +0100

    initial commit

---
+++ parser.py
@@ -0,0 +1,11 @@
+
+def random_characters_returner(number):
+    "Returns a string of character with lenght in number form provided."
+    pass
+
+def string_enconder_64(string):
+    "Returns a string encoded in base64."
+    pass
+def decoder_64(encoded_string):
+    "Returns an encoded base64 string decoded."
+    pass
