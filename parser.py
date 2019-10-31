f19d11b1e2f1faab2edd6bb6d82323f758d6fd14
SolKuczala <sol.kuczala@gmail.com>
2019-10-31 21:07:07 +0100

    commands working

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import base64
+import argparse
 from random import choice
 from string import ascii_letters
 import sys
 
 def random_characters(lenght):
-    "Returns a string of character with lenght in number form provided."
+    "Returns a string of character with the lenght of the number provided."
     string = []
     for number in range(lenght):
         string.append(choice(ascii_letters))
@@ -23,5 +24,27 @@ def decode_string_64(encoded_string):
     decodedString = base64.b64decode(encoded_string)
     return str(decodedString, "utf-8")
 
-if sys.argv:
-    print('ok')
+#mis comandos finales son:
+#grsed r {number}
+#grsed e {string}
+#grsed d {encoded string}
+#
+actions = {
+    'r' : (random_characters,int),
+    'e' : (encode_string_64,str),
+    'd' : (decode_string_64,str)
+}
+# print(actions.keys()) # devuelve array de keys
+
+input_option = sys.argv[1]
+input_value = sys.argv[2]
+
+result = None
+try:
+    function, parser = actions[input_option]
+    result = function(parser(input_value))
+except Exception as e:
+    print('invalid action')
+    exit(1)
+    
+print(result)
