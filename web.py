e75af209f6d36d25d0589458a00b3ee5e0f10c6f
SolKuczala <sol.kuczala@gmail.com>
2019-11-03 20:42:10 +0100

    flask requests

--- web.py
+++ web.py
@@ -1,8 +1,19 @@
 from flask import Flask
+import grsed_core as grsed
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/random/<int:number>")
+def lapucha(number):
+    return grsed.random_characters(number)
+   
+@app.route('/encode/<string:randomstring>')
+def lapucheta(randomstring):
+   return grsed.encode_string_64(randomstring)
+
+@app.route('/decode/<string:encodedstring>')
+def cortastetodalaloz(encodedstring):
+   return grsed.decode_string_64(encodedstring)
 
 if __name__ == "__main__":
     app.run(debug=True)
