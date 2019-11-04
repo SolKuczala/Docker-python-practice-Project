01cae8fd0e0587ce429b1948a32b470013d45bf2
SolKuczala <sol.kuczala@gmail.com>
2019-11-04 15:58:55 +0100

    split functionalities

--- web.py
+++ web.py
@@ -4,15 +4,15 @@ import grsed_core as grsed
 app = Flask(__name__)
 
 @app.route("/random/<int:number>")
-def lapucha(number):
+def x(number):
     return grsed.random_characters(number)
    
 @app.route('/encode/<string:randomstring>')
-def lapucheta(randomstring):
+def y(randomstring):
    return grsed.encode_string_64(randomstring)
 
 @app.route('/decode/<string:encodedstring>')
-def cortastetodalaloz(encodedstring):
+def z(encodedstring):
    return grsed.decode_string_64(encodedstring)
 
 if __name__ == "__main__":
